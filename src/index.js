b067958067844eb274fd36dec713ca04b942d5d1
王帅 <wangshuai_007@qq.com>
2024-06-19 20:43:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.wangshuai.app": dockerHub,
+  "quay.wangshuai.app": "https://quay.io",
+  "gcr.wangshuai.app": "https://gcr.io",
+  "k8s-gcr.wangshuai.app": "https://k8s.gcr.io",
+  "k8s.wangshuai.app": "https://registry.k8s.io",
+  "ghcr.wangshuai.app": "https://ghcr.io",
+  "cloudsmith.wangshuai.app": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
